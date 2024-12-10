023631b95b3ad2beabb7a558a8508d4591a9e3d2
kurisu-004 <1144576150@qq.com>
2024-12-11 00:17:51 +0900

    add main menu

---
+++ Scripts/DifficultyMenu.cs
@@ -0,0 +1,35 @@
+using Godot;
+using System;
+
+public partial class DifficultyMenu : Control
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+
+	private void OnEasyPressed()
+	{
+		StartGame("Easy");
+	}
+
+	private void OnMediumPressed()
+	{
+		StartGame("Medium");
+	}
+
+	private void OnHardPressed()
+	{
+		StartGame("Hard");
+	}
+
+	private void StartGame(string difficulty)
+	{
+		GetTree().ChangeSceneToFile("res://Scenes/Room.tscn");
+	}
+}
